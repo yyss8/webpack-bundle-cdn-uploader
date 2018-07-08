2198a213ed7812a25ea98c90eb75db424d146c33
Shen Chen <shen.chen@buyimp.com>
2018-07-08 12:50:50 -0400

    0.1.0改动 2018-07-08

--- README.md
+++ README.md
@@ -2,37 +2,43 @@
 
    <img src="https://github.com/yyss8/webpack-bundle-cdn-uploader/blob/master/example/output-screenshot.png" width="350">
  
-    在webpack打包完成后将打包文件上传至CDN, 开箱即用, 不需要自己配置上传功能
+    在webpack打包完成后将打包文件上传至对应CDN, 开箱即用, 不需要自己配置上传功能
 
-### 环境
+### 测试环境
 
-    只在Webpack 4测试过, webpack 3暂未测试
-
+ - webpack 3@3.11.2
+ - webpack 4@4.15.1
 
 ### 安装
 
 ```
-$ npm install --save-dev webpack-bundle-cdn-uploader
+
+npm install --save-dev webpack-bundle-cdn-uploader
+
+#webpack配置文件例子请查看example目录下的对应版本config.sample文件
+
 ```
 
 ### 支持CDN列表
 
-    暂时只支持七牛, 有时间再增加
+- 七牛 - 使用自定自己封装的上传工具
+- 腾讯cos - 使用官方SDK
 
 ### 参数
 
-- `cdn`: CDN参数, 暂时只支持七牛, 所以用七牛参数举例
-     * `type`:`qiniu` CDN名称, (必填) 其余所需参数通过这个变量来判断
-     * `bucket`: 所上传的bucket名称, 大部分存储都类似所以暂时必填
-     * `accessKey`: 七牛Access_key
-     * `secretKey`: 七牛Secret_key
-     * `host`:七牛服务器区域代码, 例如z1
-     * `test`:需要上传的文件后缀的正则, 默认(/\.css|\.js/)
-- `deletePrevious`: (true/false)是否从CDN上删除上一次上传的bundle文件 (默认false)
-- `deleteOutput`: (true/false) 是否删除webpack打包后的文件 (默认false)
+- `cdn`: CDN参数
+     * `type`:`qiniu|txcos` CDN类型, (必填) 其余所需参数通过这个变量来判断 (七牛:qiniu|腾讯txcos)
+     * `bucket`: 所上传的bucket名称, (暂时必填) 大部分对象存储提供商都类似 
+     * `test`:需要上传文件后缀的正则, 默认(/\.css|\.js/)
+     * `accessKey`: 对应的cdn access key: (必填) (七牛为access key|腾讯为secret id)
+     * `secretKey`: 对应的cdn secret key: (必填) (七牛为secret key|腾讯为secret key)
+     * `host`: 服务器区域代码, (必填) 例如七牛z1,或腾讯cos的ap-shanghai
+- `deletePrevious`: (true/false) (默认false) 是否从CDN上删除上一次上传的bundle文件, 大部分存储提供商都提供相同文件覆盖, 所以无需开启, 主要用于更换cdn后删除原cdn信息
+- `deleteOutput`: (true/false)  (默认false) 是否删除webpack打包后的文件
 
 ### 注意
 
-- 如果使用了html打包插件那么publicPath记得设置为CDN地址而非本地地址, 同时其他使用了该地址的链接也都需要替换或者上传至CDN
-- 打包结束后会在webpack配置中的输出文件夹产生一个`wp.previous.json`文件记录上一次的打包以及输出配置, 如果需要在每次打包后删除CDN上的旧资源请不要删除这个文件
-- 对打包数据显示这一块不熟悉, 所以用了本插件可能会造成output分析数据不显示, 还在想方法解决中
+- 如果使用了html打包插件那么publicPath记得设置为CDN地址而非本地目录地址, 同时其他使用了该地址的资源也都需要替换或者上传至CDN
+- 打包结束后会在webpack配置中的输出目录产生一个`wp.previous.json`文件记录上一次的打包以及输出配置, 如果需要在每次打包后删除CDN上的旧资源请不要删除这个文件
+
+### 欢迎在Issues里提各种需求和bug
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const _supportedCdns = ['qiniu'];
+const _supportedCdns = ['qiniu', 'txcos'];
 const fs = require('fs');
 
 class WebpackBundleUploaderPlugin{
@@ -21,6 +21,18 @@ class WebpackBundleUploaderPlugin{
                 this.cdn = new Qiniu( cdnObject.accessKey, cdnObject.secretKey, cdnObject.host );
 
                 return true;
+            case 'txcos':
+                if ( !cdnObject.secretKey || !cdnObject.accessKey ){
+                    return false;
+                }
+
+                const COS = require('cos-nodejs-sdk-v5');
+                this.cdn = new COS({
+                    SecretId:cdnObject.accessKey,
+                    SecretKey:cdnObject.secretKey
+                });
+
+                return true;
             case 'aliyun':
             case 'ftp':
         }
@@ -33,7 +45,37 @@ class WebpackBundleUploaderPlugin{
 
         switch ( this.cdn.constructor.name ){
             case 'Qiniu':
-                return await this.cdn.uploadByData( this.options.cdn.bucket, data, { fileName:name } );
+                try{
+                    return await this.cdn.uploadByData( this.options.cdn.bucket, data, { fileName:name } );
+                }catch(e){
+                    return e;
+                }
+            case 'COS':
+                const uploadAction = () =>{
+                    return new Promise( (resolve, reject) =>{
+                        const _data = new Buffer( data );
+    
+                        this.cdn.putObject({
+                            Bucket:this.options.cdn.bucket,
+                            Region:this.options.cdn.host,
+                            Key:name,
+                            Body:_data.toString(),
+                            ContentLength:_data.length
+                        }, (err, data)=>{
+                            if ( err ){
+                                reject();
+                                return;
+                            }
+                            resolve(data);
+                        });
+                    });
+                }
+
+                try {
+                    return await uploadAction();
+                }catch(e){
+                    return e;
+                }
         }
 
         return false;
@@ -41,17 +83,16 @@ class WebpackBundleUploaderPlugin{
     
     async deletePreviousResources( log ){
 
+        this.initUploader( log.cdn );
+
+        if ( !Array.isArray( log.files ) || log.files.length <= 0 ){
+            return {
+                error:'原记录中文件列表有误或为空, 请检查导出目录中的wp.bundle.json文件'
+            };
+        }
+
         switch ( log.cdn.type ){
             case 'qiniu':
-
-                this.initUploader( log.cdn );
-
-                if ( !Array.isArray( log.files ) || log.files.length <= 0 ){
-                    return {
-                        error:'原记录中文件列表有误或为空, 请检查导出目录中的wp.bundle.json文件'
-                    };
-                }
-
                 const resources = log.files.map( file =>{
                     return {
                         bucket:log.cdn.bucket,
@@ -64,139 +105,183 @@ class WebpackBundleUploaderPlugin{
                 }catch( e ){
                     return e;
                 }
+            case 'txcos':
+                const params = {
+                    Bucket:log.cdn.bucket,
+                    Region:log.cdn.host,
+                    Objects:log.files.map( file =>{
+                        return {
+                            Key:file.fileName
+                        };
+                    })
+                };
+
+                const deleteAction = () =>{
+                    return new Promise( (resolve,reject)=>{
+                        this.cdn.deleteMultipleObject( params , (err, data)=>{
+                            if ( err ){
+                                reject(err);
+                                return;
+                            }
+    
+                            resolve(log.files.length);
+                        });
+                    });
+                }
+
+                try {
+                    return await deleteAction();
+                }catch( e ){
+                    return e;
+                }
         }
 
         return false;
     }
 
-    apply(compiler){
+    async handleEmitted(compilation , callback = null){
 
-        compiler.hooks.afterEmit.tapAsync(
-            'BundleUploaderPlugin',
-            async (compilation) =>{
+        const outputOptions = compilation.outputOptions || compilation.options.output;
+        const { path } = outputOptions;
+        const previousPath = `${path}\\wp.previous.json`;
+        const _test = this.options.test || /\.(js|css)$/;
+            
+        if ( !(_test instanceof RegExp) ){
+            console.log('\x1b[31m', '无效test正则参数' );
+            if ( callback !== null ) callback();
+            return;
+        }
 
-                const { path } = compilation.outputOptions;
-                const previousPath = `${path}\\wp.previous.json`;
-                const _test = this.options.test || /\.(js|css)$/;
-                    
-                if ( !(_test instanceof RegExp) ){
-                    console.log('\x1b[31m', '无效test正则参数' );
+        if ( !this.options.cdn || !this.options.cdn.type || !_supportedCdns.includes( this.options.cdn.type ) ){
+            console.log('\x1b[31m', '暂不支持所选CDN');
+            if ( callback !== null ) callback();
+            return;
+        }
+
+        if ( this.options.deletePrevious ){
+            console.log( 'deletePrevious状态为开启, 正在删除CDN资源...' );
+            try {
+
+                if ( !fs.existsSync(previousPath) ){
+                    throw ( '上次上传记录不存在' );
                 }
 
-                if ( !this.options.cdn || !this.options.cdn.type || !_supportedCdns.includes( this.options.cdn.type ) ){
-                    console.log('\x1b[31m', '暂不支持所选CDN');
+                const previousLog = fs.readFileSync( previousPath, 'utf8' );
+
+                if ( previousLog && previousLog.Error ){
+                    throw ( previousLog.Error );   
                 }
 
-                if ( this.options.deletePrevious ){
-                    console.log( 'deletePrevious状态为开启, 正在删除CDN资源...' );
-                    try {
+                const _log = JSON.parse( previousLog );
 
-                        if ( !fs.existsSync(previousPath) ){
-                            throw ( '上次上传记录不存在' );
-                        }
+                const cdnDeleteResponse = await this.deletePreviousResources( _log );
 
-                        const previousLog = fs.readFileSync( previousPath, 'utf8' );
-
-                        if ( previousLog && previousLog.Error ){
-                            throw ( previousLog.Error );   
-                        }
-
-                        const _log = JSON.parse( previousLog );
-
-                        const cdnDeleteResponse = await this.deletePreviousResources( _log );
-
-                        if ( cdnDeleteResponse && typeof cdnDeleteResponse.error !== 'undefined' ){
-                            throw( cdnDeleteResponse.error );
-                        }
-
-                        if ( cdnDeleteResponse === false ){
-                            throw ('上一次CDN上传记录有误, 找不到可支持CDN, 请检查导出目录中的wp.bundle.json文件');
-                        }
-
-                        console.log( '\x1b[32m', `成功删除${cdnDeleteResponse}个上次上传bundle文件` );
-
-                        fs.unlinkSync( previousPath );
-                    }catch( e ){
-                        console.log('\x1b[31m', `跳过删除上一次上传文件, 因为:${e}`);
-                    }
+                if ( cdnDeleteResponse && typeof cdnDeleteResponse.error !== 'undefined' ){
+                    throw( cdnDeleteResponse.error );
                 }
 
-                const uploader = this.initUploader( this.options.cdn );
-
-                if( !uploader ){
-                    console.log('\x1b[31m', '加载CDN对象失败, 请检查CDN参数是否完整');
-                    return;
+                if ( cdnDeleteResponse === false ){
+                    throw ('上一次CDN上传记录有误, 找不到可支持CDN, 请检查导出目录中的wp.bundle.json文件');
                 }
 
-                let previousOutput = {
-                    cdn:this.options.cdn,
-                    files:[]
-                }
-                let uploaderError = false;
+                console.log( '\x1b[32m', `成功删除${cdnDeleteResponse}个上次上传bundle文件` );
 
-                console.log('\x1b[32m', '开始上传Bundle文件至CDN...');
-
-                for ( let asset in compilation.assets ){
-
-                    const { existsAt, _value, _name } = compilation.assets[asset];
-                    if ( !_test.test( existsAt ) ){
-                        continue;
-                    }
-
-                    let response, fileName;
-                
-                    try {
-                        //非js文件不会直接包括文件内容, 需要使用fs提取内容并手动提取文件名
-                        if ( typeof _name === 'undefined' ){
-                            fileName = existsAt.replace(`${path}\\`, '').replace(/\\/g, '/');
-                            const fileContent = fs.readFileSync( existsAt,'utf8');
-                            response = await this.upload( fileContent, fileName );
-                        }else{
-                            fileName = _name;
-                            response = await this.upload( _value, _name );
-                        }
-
-                        if ( this.options.deleteOutput ){
-                            fs.unlinkSync( `${path}\\${fileName}` );   
-                        }
-
-                        previousOutput.files.push( {fileName} );
-
-                        console.log( `文件${fileName}上传完成`);
-                    }catch( e ){
-                        uploaderError = true;
-                        console.log('\x1b[31m',`读取文件${fileName}出错: ${e}, 上传中断请尝试重新打包`);
-                        break;
-                    }
-
-                    //一个文件出错则停止所有上传
-                    if ( typeof response.error !== 'undefined' ){
-                        uploaderError = true;
-                        console.log('\x1b[31m', `CDN上传出错 : ${response.error}, 上传中断请尝试重新打包`);
-                        break;
-                    }
-                    
-                    
-                }
-
-                //保存上传记录, 用于下一次删除
-                if ( previousOutput.files.length > 0 ){
-                    fs.writeFileSync( previousPath, JSON.stringify( previousOutput ));
-                }
-
-                if ( !uploaderError ){
-
-                    console.log('\x1b[0m', '所有文件已上传至CDN');
-
-                    if ( this.options.deleteOutput ){
-                        console.log('deleteOutput状态为启用, 所以所有bundle文件已删除');
-                    }
-                }else{
-                    console.log('\x1b[0m', '上传中断');
-                }
-                
+                fs.unlinkSync( previousPath );
+            }catch( e ){
+                console.log('\x1b[31m', `跳过删除上一次上传文件, 因为:${e}`);
             }
-        )
+        }
+
+        const uploader = this.initUploader( this.options.cdn );
+
+        if( !uploader ){
+            console.log('\x1b[31m', '加载CDN对象失败, 请检查CDN参数是否完整');
+            if ( callback !== null ) callback();
+            return;
+        }
+
+        let previousOutput = {
+            cdn:this.options.cdn,
+            files:[]
+        }
+        let uploaderError = false;
+
+        console.log('\x1b[32m', '开始上传Bundle文件至CDN...');
+
+        for ( let asset in compilation.assets ){
+
+            const { existsAt, _value, _name } = compilation.assets[asset];
+            if ( !_test.test( existsAt ) ){
+                continue;
+            }
+
+            let response, fileName;
+        
+            try {
+                //非js文件不会直接包括文件内容, 需要使用fs提取内容并手动提取文件名
+                if ( typeof _name === 'undefined' ){
+                    fileName = existsAt.replace(`${path}\\`, '').replace(/\\/g, '/');
+                    const fileContent = fs.readFileSync( existsAt,'utf8');
+                    response = await this.upload( fileContent, fileName );
+                }else{
+                    fileName = _name;
+                    response = await this.upload( _value, _name );
+                }
+
+                if ( this.options.deleteOutput ){
+                    fs.unlinkSync( `${path}\\${fileName}` );   
+                }
+
+                previousOutput.files.push( {fileName} );
+
+                console.log( `文件${fileName}上传完成`);
+            }catch( e ){
+                uploaderError = true;
+                console.log('\x1b[31m',`读取文件${fileName}出错: ${e}, 上传中断请尝试重新打包`);
+                break;
+            }
+
+            //一个文件出错则停止所有上传
+            if ( typeof response.error !== 'undefined' ){
+                uploaderError = true;
+                console.log('\x1b[31m', `CDN上传出错 : ${response.error}, 上传中断请尝试重新打包`);
+                break;
+            }
+            
+            
+        }
+
+        //保存上传记录, 用于下一次删除
+        if ( previousOutput.files.length > 0 ){
+            fs.writeFileSync( previousPath, JSON.stringify( previousOutput ));
+        }
+
+        if ( !uploaderError ){
+
+            console.log('\x1b[0m', '所有文件已上传至CDN');
+
+            if ( this.options.deleteOutput ){
+                console.log('deleteOutput状态为启用, 所以所有bundle文件已删除');
+            }
+        }else{
+            console.log('\x1b[0m', '上传中断');
+        }
+
+        if ( callback !== null ) callback();
+
+    }
+
+    apply(compiler){
+        
+        //webpack版本兼容
+        if ( compiler.hooks ){ //webpack 4
+            compiler.hooks.afterEmit.tapAsync(
+                'BundleUploaderPlugin',
+                this.handleEmitted.bind(this)
+            )
+        }else{ //webpack 3
+            compiler.plugin('after-emit', this.handleEmitted.bind(this));
+        }
 
     }
 
